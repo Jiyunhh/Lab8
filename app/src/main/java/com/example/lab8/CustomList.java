7433fdc2fd82de90cca8eed444b9ef72e3fe7025
Jiyunhh <nathanphan193@gmail.com>
2023-03-09 19:03:47 -0700

    CountCityPass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -72,7 +72,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public int countCities() {
-        //return cities.size();
+        return cities.size();
     }
 
 }
